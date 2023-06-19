5f2d841841eb231ca9be4b87d3ef686d3928cae4
nnosach <n.nosach@inxy.com>
2023-06-19 12:53:10 +0300

    installed Vue/Vuex, SCSS, created store

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        // your data here
+    },
+    mutations: {
+        // your mutation here
+    },
+    actions: {
+        // your action here
+    },
+    getters: {
+        // your getters here
+    }
+});
+
+export default store;
