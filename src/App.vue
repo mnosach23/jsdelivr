7e5ad2718cb02fcbacbaf604211ca6a53f84b04a
nnosach <n.nosach@inxy.com>
2023-06-19 13:04:47 +0300

    installed Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <div id="app">
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <h1>Frozeneon</h1>
+    </v-app-bar>
 
-  </div>
+    <v-main>
+      main
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 export default {
   name: 'App',
-}
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style lang="scss">
-//
-</style>
